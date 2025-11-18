7e07e68937dbf57072a7bf91814ab7be18dd2971
Laszlo Uveges <laszlo@giantswarm.io>
2025-11-18 15:04:42 +0100

    Skip installing cert-manager as it is obsolete and does not work anymore

--- integration/test/basic/basic_test.go
+++ integration/test/basic/basic_test.go
@@ -7,8 +7,6 @@ import (
 	"context"
 	"testing"
 
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-
 	"github.com/giantswarm/apptest"
 	"github.com/giantswarm/apptest/integration/env"
 	"github.com/giantswarm/apptest/integration/setup"
@@ -35,14 +33,6 @@ func TestBasic(t *testing.T) {
 	ctx := context.Background()
 
 	apps := []apptest.App{
-		{
-			// Install a dependency for the component being tested.
-			CatalogName:   "default", // Production catalog.
-			Name:          "cert-manager-app",
-			Namespace:     metav1.NamespaceSystem,
-			Version:       "2.3.1", // Specify the version you need.
-			WaitForDeploy: true,
-		},
 		{
 			// Install the component being tested.
 			CatalogName:   "control-plane-test-catalog", // Test catalog.
